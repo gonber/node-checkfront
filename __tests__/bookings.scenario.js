0cad8cf3bb8eefb946f3a5feec06924fb1a740a0
gonber <goncalobernardo@gmail.com>
2019-12-21 15:10:27 +1100

    feat: bookings.list() supports relative dates

--- __tests__/bookings.scenario.js
+++ __tests__/bookings.scenario.js
@@ -28,6 +28,26 @@ test('lists bookings with params', async () => {
   expect(bookings).toHaveLength(0);
 });
 
+test.skip('lists bookings with date params', async () => {
+  const [{ created_date: createdDate }] = await checkfront.bookings.list();
+  const paramsAt = { created_date: createdDate };
+  const paramsBefore = {
+    created_date: {
+      date: createdDate + 60,
+      relative: 'before',
+    },
+  };
+  const paramsAfter = {
+    created_date: {
+      date: createdDate + 60,
+      relative: 'after',
+    },
+  };
+  expect(await checkfront.bookings.list(paramsAt)).toHaveLength(1);
+  expect(await checkfront.bookings.list(paramsBefore)).toHaveLength(1);
+  expect(await checkfront.bookings.list(paramsAfter)).toHaveLength(0);
+});
+
 test('retrieves booking', async () => {
   const booking = await checkfront.bookings.retrieve(1);
   expect(booking.booking_id).toEqual(1);
